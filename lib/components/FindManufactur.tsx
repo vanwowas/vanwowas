bb16b8593950a1f1344805ec69d8233eff70c479
Simon Ruisinger <sruisinger@cellular.de>
2022-02-18 21:14:09 +0100

    add search button

--- lib/components/FindManufactur.tsx
+++ lib/components/FindManufactur.tsx
@@ -5,6 +5,7 @@ import colors from '../style/colors'
 import { stack } from '../style/mixins'
 import { Headline1 } from '../style/typography'
 import { Place } from '../types/db'
+import Button from './Button'
 import Input from './Input'
 
 const Container = styled.div`
@@ -48,21 +49,23 @@ const FindManufactur: React.FC<Props> = ({ onChange }) => {
                 )
                 if (newPlace && newPlace !== place) {
                     setPlace(newPlace)
-                    onChange({ price: price, place: newPlace })
                 }
             } catch {}
         },
-        [onChange, place, price]
+        [place]
     )
 
     const handlePriceChange = useCallback(
         async (e: ChangeEvent<HTMLInputElement>) => {
             const newPrice = e.target.value
             setPrice(Number(newPrice))
-            onChange({ price: Number(newPrice), place })
         },
-        [onChange, place]
+        []
     )
+
+    const handleSearch = useCallback(() => {
+        onChange({ price, place })
+    }, [onChange, place, price])
 
     return (
         <Container>
@@ -79,6 +82,14 @@ const FindManufactur: React.FC<Props> = ({ onChange }) => {
                     maxLength={5}
                     type="text"
                 />
+                <Button
+                    borderColor="dark"
+                    color="light"
+                    backgroundColor="primary"
+                    onClick={handleSearch}
+                >
+                    suchen
+                </Button>
             </InputWrapper>
         </Container>
     )
